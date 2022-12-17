4fcd41b4f66aa5f1dd8240006d399accc58b66d0
Debasis Nayak <debasisnayak@Debasiss-MacBook-Pro.local>
2022-12-17 18:12:48 +0530

    initial commit

---
+++ src/data/index.js
@@ -0,0 +1,85 @@
+const employeesData = [
+    {
+        id: 1,
+        firstName: 'Debasis',
+        lastName: 'Nayak',
+        email: 'debasishnayak0739@gmail.com',
+        salary: '95000',
+        date: '2019-04-11'
+    },
+    {
+        id: 2,
+        firstName: 'Ranandan',
+        lastName: 'Mahto',
+        email: 'rajrx100@gmail.com',
+        salary: '85000',
+        date: '2019-04-18'
+    },
+    {
+        id: 3,
+        firstName: 'Rajesh',
+        lastName: 'Mardi',
+        email: 'rajeshjoseph@gmail.com',
+        salary: '79000',
+        date: '2019-05-01'
+    },
+    {
+        id: 4,
+        firstName: 'Ritesh',
+        lastName: 'Mahto',
+        email: 'ritesh07@gmail.com',
+        salary: '65000',
+        date: '2019-05-11'
+    },
+    {
+        id: 5,
+        firstName: 'Bikram',
+        lastName: 'Soren',
+        email: 'bikram@gmail.com',
+        salary: '55000',
+        date: '2020-01-12'
+    },
+    {
+        id: 6,
+        firstName: 'Alok',
+        lastName: 'Raj',
+        email: 'alokraj@gmail.com',
+        salary: '100000',
+        date: '2019-06-20'
+    },
+    {
+        id: 7,
+        firstName: 'viku',
+        lastName: 'Gupta',
+        email: 'vikkmar@gmail.com',
+        salary: '89000',
+        date: '2020-06-30'
+    },
+    {
+        id: 8,
+        firstName: 'Bhargav',
+        lastName: 'Bera',
+        email: 'bhargav@gmail.com',
+        salary: '85000',
+        date: '2020-07-18'
+    },
+    {
+        id: 9,
+        firstName: 'Adarsh',
+        lastName: 'parshad',
+        email: 'adarshjordan@gmail.com',
+        salary: '72000',
+        date: '2022-02-11'
+    },
+    {
+        id: 10,
+        firstName: 'Rahul',
+        lastName: 'Patra',
+        email: 'rahul@gmail.com',
+        salary: '79000',
+        date: '2022-03-17'
+    },
+
+];
+
+export { employeesData };
